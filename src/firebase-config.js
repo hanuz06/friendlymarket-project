81bd217e2a2d06830a9b8a8fdb9d974e4b97aa42
hanuz06 <lee1975uz@gmail.com>
2020-03-29 19:56:01 -0600

    Add firebase config code

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@
   https://firebase.google.com/docs/web/setup#config-object
 */
 const firebaseConfig = {
-  apiKey: 'TODO',
-  authDomain: 'TODO',
-  databaseURL: 'TODO',
-  projectId: 'TODO',
-  storageBucket: 'TODO',
-  messagingSenderId: 'TODO',
-  appId: 'TODO'
+  apiKey: "AIzaSyBBth9K81RsmdxFUY2Bn4nq6LuZsPTBoWI",
+  authDomain: "friendlymarket-e9b2c.firebaseapp.com",
+  databaseURL: "https://friendlymarket-e9b2c.firebaseio.com",
+  projectId: "friendlymarket-e9b2c",
+  storageBucket: "friendlymarket-e9b2c.appspot.com",
+  messagingSenderId: "253371364599",
+  appId: "1:253371364599:web:d366de2f75605ffcee0ff2"
 };
 
 export default firebaseConfig;
